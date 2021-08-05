b105c966eedfd2c90323c60a5863bc538fffa959
Souryayan Banerjee <abesh2107@gmail.com>
2021-08-05 13:46:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+var randomNumber2 = Math.random();
+randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+var dice1 = "images/dice" + randomNumber1 + ".png";
+var dice2 = "images/dice" + randomNumber2 + ".png";
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src",dice1);
+var image2 = document.querySelector(".img2");
+image2.setAttribute("src",dice2);
+if (randomNumber1 > randomNumber2)
+document.querySelector("h1").innerHTML = "Player 1 wins";
+else if (randomNumber1 < randomNumber2)
+document.querySelector("h1").innerHTML = "Player 2 wins";
+else
+document.querySelector("h1").innerHTML = "Its a tie. Refresh again!";
